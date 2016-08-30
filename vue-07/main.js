6785dc0100b862e86c41fb514ea0dc19bd24acbf
Jhonatan Gonzalez <jgonzalez@hugeinc.com>
2016-08-29 22:05:57 -0500

    WIP | Vue ex 07 - Rendering & Working with lists

---
+++ vue-07/main.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    tasks: [
+      { body: 'Go to the store', done: false },
+      { body: 'Go to the bank', done: false },
+      { body: 'Go to the doctor', done: true },
+    ]
+  }
+});
