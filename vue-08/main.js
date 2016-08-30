311dba6c43b421c0ada1a50ad7eefaea968b5fe7
Jhonatan Gonzalez <jhesgodi@gmail.com>
2016-08-29 23:45:44 -0500

    Vue ex 08 - Easy todo list

--- vue-08/main.js
+++ vue-08/main.js
@@ -2,8 +2,39 @@ Vue.component('tasks', {
   template: '#tasks-template',
   props: ['list'],
   methods: {
+    addTask: function () {
+      if (this.newTask) {
+        this.list.push(this.createTask(this.newTask));
+        this.newTask = null;
+      }
+    },
     toggleDoneFor: function (task) {
       task.done = !task.done;
+    },
+    isDone: function (task) {
+      return task.done;
+    },
+    inProgress: function (task) {
+      return !this.isDone(task);
+    },
+    removeTask: function (task) {
+      this.list.$remove(task);
+    },
+    clearDone: function () {
+      this.list = this.list.filter(this.inProgress);
+    },
+    createTask: function (body) {
+      return { body: body.trim(), done: false };
+    }
+  },
+  data: function () {
+    return {
+      newTask: null
+    };
+  },
+  computed: {
+    remaining: function () {
+      return this.list.filter(this.inProgress).length;
     }
   }
 });
